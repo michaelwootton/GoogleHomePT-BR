7326eab46a1e5a6812c707d8a05a5f0bcf23d5b4
michaelwootton <30409710+michaelwootton@users.noreply.github.com>
2019-04-15 18:28:18 -0300

    Inicial do Webhook 2

---
+++ service.js
@@ -0,0 +1,49 @@
+const OracleBot = require('@oracle/bots-node-sdk');
+const { WebhookClient, WebhookEvent } = OracleBot.Middleware;
+const bodyParser = require('body-parser');
+const { dialogflow } = require('actions-on-google');
+const assistant = dialogflow();
+
+module.exports = (app) => {
+  const logger = console;
+  OracleBot.init(app, {
+    logger,
+  });
+
+  const webhook = new WebhookClient({
+    channel: {
+      url: 'http://bots-connectors:8000/connectors/v1/tenants/chatbot-tenant/listeners/webhook/channels/291868e7-1eeb-490d-9fe5-c84362f34492',
+      secret: 'BpZMnlY64tzVoBZHRtcgNvvs90ZE8lN6',
+    }
+  });
+
+  webhook
+    .on(WebhookEvent.ERROR, err => logger.error('Error:', err.message))
+    .on(WebhookEvent.MESSAGE_SENT, message => logger.info('Message to chatbot:', message))
+    .on(WebhookEvent.MESSAGE_RECEIVED, message => logger.info('Message from chatbot:', message))
+
+  
+  assistant.intent('Default Fallback Intent', (conv) => {
+    logger.info('Got query : ', conv.query);
+    const promise = new Promise(function (resolve, reject) {
+      const MessageModel = webhook.MessageModel();
+      const message = {
+        userId: 'anonymous',
+        messagePayload: MessageModel.textConversationMessage(conv.query)
+      };
+      webhook.send(message);
+      webhook.on(WebhookEvent.MESSAGE_RECEIVED, message => {
+        resolve(message);
+      });
+    })
+      .then(function (result) {
+          conv.ask(result.messagePayload.text);
+        })
+    return promise;
+  })
+  
+  app.post('/bot/message', webhook.receiver());
+
+  app.use('/fulfillment',bodyParser.json(),assistant);
+  app.post('/fulfillment', assistant );
+}
