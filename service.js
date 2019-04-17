18d0ceb840acd7fcb821633f42905f5bdaa7e3cb
michaelwootton <30409710+michaelwootton@users.noreply.github.com>
2019-04-17 17:32:12 -0300

    adicionando trata usuario

--- service.js
+++ service.js
@@ -64,9 +64,9 @@ module.exports = (app) => {
     // se l'input e' fine usa il metodo tell() che risponde e chiude la connessione
     if (signin.status === 'OK') {
      //   userpayload = conv.user.profile.payload;
-        conv.ask('Agora eu tenho os seus detalhes, ${userpayload.name}. O que voce quer fazer a seguir?');
-        logger.info('Account Linking rolou, dados de profile são: ', signin);
-        logger.info('Estes são os dados do Conv: ', conv);
+     logger.info('Account Linking rolou, dados de profile são: ', JSON.stringify(signin));
+     logger.info('Estes são os dados do Conv: ', JSON.stringify(conv.user.profile.payload));
+     conv.ask('Agora eu tenho os seus detalhes, ${userpayload.name}. O que voce quer fazer a seguir?');
     } else {
         conv.ask('Não poderei Salvar seus dados servidor EPM e nome, mas o que vc deseja fazer a seguir');
         logger.info('Account Linking Não rolou');
